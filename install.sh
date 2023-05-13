181acdf3134fc4a73249cd2e973a3ef2241a7665
Jeremy Soller <jackpot51@gmail.com>
2023-05-12 20:40:16 -0600

    Improve install script

--- install.sh
+++ install.sh
@@ -6,10 +6,19 @@ cd "$(dirname "$0")"
 
 cargo build --release
 
+# Copy config file
 sudo cp "${HOSTNAME}.toml" /etc/evremap.toml
-sudo cp target/release/evremap /usr/bin/evremap
-sudo cp evremap.service /usr/lib/systemd/system/evremap.service
 
+# Copy and reload service
+sudo cp evremap.service /usr/lib/systemd/system/evremap.service
 sudo systemctl daemon-reload
+
+# Stop service if running
+sudo systemctl stop evremap.service
+
+# Copy new binary
+sudo cp target/release/evremap /usr/bin/evremap
+
+# Enable and start service
 sudo systemctl enable evremap.service
 sudo systemctl start evremap.service
