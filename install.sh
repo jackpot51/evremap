5e7ac2273c4243423eb364eaccaa6fc4abc96d1e
Jeremy Soller <jackpot51@gmail.com>
2023-05-12 20:37:20 -0600

    Add jeremy-sunrise config and install script

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -ex
+
+cargo build --release
+
+sudo cp "${HOSTNAME}.toml" /etc/evremap.toml
+sudo cp target/release/evremap /usr/bin/evremap
+sudo cp evremap.service /usr/lib/systemd/system/evremap.service
+
+sudo systemctl daemon-reload
+sudo systemctl enable evremap.service
+sudo systemctl start evremap.service
